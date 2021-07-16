f1d6bb1e36f475157eb372d787e950bad49a13fc
talentedev <33281524+talentedev@users.noreply.github.com>
2021-07-17 00:06:11 +0900

    fix issue

--- profile/wp-content/themes/empathy/js/main.js
+++ profile/wp-content/themes/empathy/js/main.js
@@ -179,57 +179,57 @@ var portfolioKeyword;
 			https://developers.google.com/maps/documentaztion/javascript/examples/marker-simple
 		*/
 		// When the window has finished loading create our google map below
-		google.maps.event.addDomListener(window, 'load', initializeMap);
+		// google.maps.event.addDomListener(window, 'load', initializeMap);
 	
-		function initializeMap() {
+		// function initializeMap() {
 			
-			var mapCanvas = $('#map-canvas');
+		// 	var mapCanvas = $('#map-canvas');
 			
-			if(mapCanvas.length) {
-				var latitude = mapCanvas.data("latitude");
-				var longitude = mapCanvas.data("longitude");
-				var zoom = mapCanvas.data("zoom");
-				var marker_image = mapCanvas.data("marker-image");
+		// 	if(mapCanvas.length) {
+		// 		var latitude = mapCanvas.data("latitude");
+		// 		var longitude = mapCanvas.data("longitude");
+		// 		var zoom = mapCanvas.data("zoom");
+		// 		var marker_image = mapCanvas.data("marker-image");
 				
-				// Basic options for a simple Google Map
-				// For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
-				var mapOptions = {
+		// 		// Basic options for a simple Google Map
+		// 		// For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+		// 		var mapOptions = {
 					
-					// How zoomed in you want the map to start at (always required)
-					zoom: zoom,
+		// 			// How zoomed in you want the map to start at (always required)
+		// 			zoom: zoom,
 					
-					// disable zoom controls
-					disableDefaultUI: true,
+		// 			// disable zoom controls
+		// 			disableDefaultUI: true,
 	
-					// The latitude and longitude to center the map (always required)
-					center: new google.maps.LatLng(latitude,longitude),
+		// 			// The latitude and longitude to center the map (always required)
+		// 			center: new google.maps.LatLng(latitude,longitude),
 	
-					// How you would like to style the map. 
-					// This is where you would paste any style found on Snazzy Maps.
-					styles: [{"featureType":"administrative.locality","elementType":"all","stylers":[{"hue":"#2c2e33"},{"saturation":7},{"lightness":19},{"visibility":"on"}]},{"featureType":"landscape","elementType":"all","stylers":[{"hue":"#ffffff"},{"saturation":-100},{"lightness":100},{"visibility":"simplified"}]},{"featureType":"poi","elementType":"all","stylers":[{"hue":"#ffffff"},{"saturation":-100},{"lightness":100},{"visibility":"off"}]},{"featureType":"road","elementType":"geometry","stylers":[{"hue":"#bbc0c4"},{"saturation":-93},{"lightness":31},{"visibility":"simplified"}]},{"featureType":"road","elementType":"labels","stylers":[{"hue":"#bbc0c4"},{"saturation":-93},{"lightness":31},{"visibility":"on"}]},{"featureType":"road.arterial","elementType":"labels","stylers":[{"hue":"#bbc0c4"},{"saturation":-93},{"lightness":-2},{"visibility":"simplified"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"hue":"#e9ebed"},{"saturation":-90},{"lightness":-8},{"visibility":"simplified"}]},{"featureType":"transit","elementType":"all","stylers":[{"hue":"#e9ebed"},{"saturation":10},{"lightness":69},{"visibility":"on"}]},{"featureType":"water","elementType":"all","stylers":[{"hue":"#e9ebed"},{"saturation":-78},{"lightness":67},{"visibility":"simplified"}]}]
-				};
+		// 			// How you would like to style the map. 
+		// 			// This is where you would paste any style found on Snazzy Maps.
+		// 			styles: [{"featureType":"administrative.locality","elementType":"all","stylers":[{"hue":"#2c2e33"},{"saturation":7},{"lightness":19},{"visibility":"on"}]},{"featureType":"landscape","elementType":"all","stylers":[{"hue":"#ffffff"},{"saturation":-100},{"lightness":100},{"visibility":"simplified"}]},{"featureType":"poi","elementType":"all","stylers":[{"hue":"#ffffff"},{"saturation":-100},{"lightness":100},{"visibility":"off"}]},{"featureType":"road","elementType":"geometry","stylers":[{"hue":"#bbc0c4"},{"saturation":-93},{"lightness":31},{"visibility":"simplified"}]},{"featureType":"road","elementType":"labels","stylers":[{"hue":"#bbc0c4"},{"saturation":-93},{"lightness":31},{"visibility":"on"}]},{"featureType":"road.arterial","elementType":"labels","stylers":[{"hue":"#bbc0c4"},{"saturation":-93},{"lightness":-2},{"visibility":"simplified"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"hue":"#e9ebed"},{"saturation":-90},{"lightness":-8},{"visibility":"simplified"}]},{"featureType":"transit","elementType":"all","stylers":[{"hue":"#e9ebed"},{"saturation":10},{"lightness":69},{"visibility":"on"}]},{"featureType":"water","elementType":"all","stylers":[{"hue":"#e9ebed"},{"saturation":-78},{"lightness":67},{"visibility":"simplified"}]}]
+		// 		};
 	
-				// Get the HTML DOM element that will contain your map 
-				// We are using a div with id="map" seen below in the <body>
-				var mapElement = document.getElementById('map-canvas');
-				//var mapElement = $('#map-canvas');
-				//var myLatlng = new google.maps.LatLng(mapElement.data("latitude"),mapElement.data("longitude"));
+		// 		// Get the HTML DOM element that will contain your map 
+		// 		// We are using a div with id="map" seen below in the <body>
+		// 		var mapElement = document.getElementById('map-canvas');
+		// 		//var mapElement = $('#map-canvas');
+		// 		//var myLatlng = new google.maps.LatLng(mapElement.data("latitude"),mapElement.data("longitude"));
 				
-				// Create the Google Map using our element and options defined above
-				var map = new google.maps.Map(mapElement, mapOptions);
+		// 		// Create the Google Map using our element and options defined above
+		// 		var map = new google.maps.Map(mapElement, mapOptions);
 	
-				//CREATE A CUSTOM PIN ICON
-				var marker_image = marker_image;
-				var pinIcon = new google.maps.MarkerImage(marker_image,null,null, null,new google.maps.Size(120, 90));    
+		// 		//CREATE A CUSTOM PIN ICON
+		// 		var marker_image = marker_image;
+		// 		var pinIcon = new google.maps.MarkerImage(marker_image,null,null, null,new google.maps.Size(120, 90));    
 			
-				var marker = new google.maps.Marker({
-				   position: new google.maps.LatLng(latitude,longitude),
-				  map: map,
-				  icon: pinIcon,
-				  title: 'Hey, I am here'
-				});
-			}
-		}
+		// 		var marker = new google.maps.Marker({
+		// 		   position: new google.maps.LatLng(latitude,longitude),
+		// 		  map: map,
+		// 		  icon: pinIcon,
+		// 		  title: 'Hey, I am here'
+		// 		});
+		// 	}
+		// }
 		// ------------------------------
 				
 		
